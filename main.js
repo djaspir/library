fd42585aa80aca00c90e868dc36d19b32def679d
djaspir <jaspersanchez1996@gmail.com>
2022-08-24 09:38:14 +0800

    Add delete book function

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
 const addBtn = document.getElementById("addBtn");
 const confirmBtn = document.getElementById("confirmBtn");
-
 const modal = document.getElementById("modal");
 const bookForm = document.getElementById("bookForm");
-
-const title = document.getElementById("title");
-const author = document.getElementById("author");
-const pages = document.getElementById("pages");
-const isRead = document.getElementById("isRead");
-
 const bookList = document.getElementById("bookList");
 
 let myLibrary = [];
@@ -21,21 +14,49 @@ function Book(title, author, pages, isRead) {
 }
 
 function addBookToLibrary() {
-  const row = document.createElement("tr");
-
-  row.dataset.id = myLibrary.length;
+  const title = document.getElementById("title");
+  const author = document.getElementById("author");
+  const pages = document.getElementById("pages");
+  const isRead = document.getElementById("isRead");
 
   myLibrary.push(
     new Book(title.value, author.value, pages.value, isRead.value)
   );
+}
 
-  Object.values(myLibrary[myLibrary.length - 1]).forEach((item) => {
-    const cell = document.createElement("td");
-    cell.textContent = item;
-    row.appendChild(cell);
+function displayBook() {
+  myLibrary.forEach((key, index) => {
+    const row = document.createElement("tr");
+    row.dataset.id = index;
+
+    Object.values(key).forEach((val) => {
+      const cell = document.createElement("td");
+      const content = document.createTextNode(val);
+      cell.appendChild(content);
+      row.appendChild(cell);
+    });
+    const cellBtn = document.createElement("td");
+    const delBtn = document.createElement("button");
+    delBtn.setAttribute("id", "delBtn");
+    delBtn.textContent = `Remove`;
+    delBtn.addEventListener("click", () => {
+      removeBook(row.dataset.id);
+      displayReset();
+      displayBook();
+    });
+
+    cellBtn.appendChild(delBtn);
+    row.appendChild(cellBtn);
+    bookList.appendChild(row);
   });
+}
 
-  bookList.appendChild(row);
+function removeBook(data) {
+  delete myLibrary[data];
+}
+
+function displayReset() {
+  bookList.innerHTML = "";
 }
 
 addBtn.addEventListener("click", () => {
@@ -45,4 +66,6 @@ addBtn.addEventListener("click", () => {
 
 confirmBtn.addEventListener("click", () => {
   addBookToLibrary();
+  displayReset();
+  displayBook();
 });
