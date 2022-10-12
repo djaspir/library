8fc52360db4a7e5f97b09b3199b6962c7f6779b0
djaspir <jaspersanchez1996@gmail.com>
2022-10-12 14:03:54 +0800

    Refactor to Class

--- main.js
+++ main.js
@@ -6,11 +6,13 @@ const bookList = document.getElementById("bookList");
 
 let myLibrary = [];
 
-function Book(title, author, pages, isRead) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.isRead = isRead;
+class Book {
+  constructor(title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+  }
 }
 
 function removeBook(data) {
